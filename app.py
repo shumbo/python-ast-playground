2b8a22ab9a4ccd59bf0213d12a4b31e8a356be3c
Shun Kashiwa <shunthedev@gmail.com>
2025-04-01 21:26:35 -0700

    add json view

--- app.py
+++ app.py
@@ -3,6 +3,21 @@ import ast
 import streamlit as st
 
 
+def ast_to_dict(node):
+    """Convert an AST node to a dictionary."""
+    if isinstance(node, ast.AST):
+        return {
+            "type": type(node).__name__,
+            **{k: ast_to_dict(v) for k, v in ast.iter_fields(node)},
+            "lineno": getattr(node, "lineno", None),
+            "col_offset": getattr(node, "col_offset", None),
+        }
+    elif isinstance(node, list):
+        return [ast_to_dict(n) for n in node]
+    else:
+        return node
+
+
 def main():
     st.title("Python AST Playground")
 
@@ -18,12 +33,26 @@ def main():
             # Parse the code to get the AST
             parsed_ast = ast.parse(code)
 
-            # Display the AST dump
-            st.subheader("AST Dump:")
-            st.code(ast.dump(parsed_ast, indent=2), language="text")
-
             # Optionally show a success message
             st.success("AST generated successfully!")
+
+            dump, tree = st.tabs(
+                [
+                    "Dump",
+                    "Tree",
+                ]
+            )
+
+            with dump:
+                # Display the AST dump
+                st.subheader("AST Dump:")
+                st.code(ast.dump(parsed_ast, indent=2), language="text")
+
+            with tree:
+                # Display the AST tree
+                st.subheader("AST Tree:")
+                st.json(ast_to_dict(parsed_ast), expanded=False)
+
         except SyntaxError as e:
             st.error(f"Syntax Error: {e}")
         except Exception as e:
